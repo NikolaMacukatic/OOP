692f99978b27d391c93b040633085b9b5ef34e19
NikolaMacukatic <nikolamacukatic123@gmail.com>
2025-10-26 17:10:12 +0100

    Vjezba1.cpp

--- Vjezba1.cpp
+++ Vjezba1.cpp
@@ -1,62 +1,126 @@
 #include <iostream>
 #include <iomanip>
+#include <string>
+#include <cctype>
 using namespace std;
-
 
 int ogr(int x, int low = 0, int high = 100)
 {
-	if (x <= low) return low;
-	else return high;
-
+    if (x < low) return low;
+    else if (x > high) return high;
+    else return x;
 }
 
 double ogr(double x, double low = 0.0, double high = 1.0)
 {
-	if (x <= low) return low;
-	else return high;
+    if (x < low) return low;
+    else if (x > high) return high;
+    else return x;
+}
+
+int& at(int ary[], int i)
+{
+    return ary[i];
 
 }
 
 int main()
-{
-	int a;
-	double b;
+{       
+    
+    //ZAD2
+    int a;
+    double b;
 
-	cout << "Unesi prvo int a pa double b" << endl;
-	cin >> a;
-	cin >> b;
-	cout << "unutar intervala broj " << a << "vraca: " << ogr(a) << endl;
-	cout << "unutar intervala broj " << b << "vraca: " << ogr(b) << endl;
+    cout << "Unesi int i double b: " << endl;
+    cin >> a;
+    cin >> b;
 
+    cout << "Broj " << a << " vraca: " << ogr(a) << endl;
+    cout << "broj" << b << " vraca: " << ogr(b) << endl;
+
+    cout << "ogr -5 = " << ogr(-5) << endl;
+    cout << "ogr 120 = " << ogr(120) << endl;
+    cout << "ogr -1.5 = " << ogr(1.5) << endl;
+
+
+    //ZAD3
+
+    string s;
+    cin.get();
+    cout << "Unesi string" << endl;
+    getline(cin, s);
+
+    for (char& c : s)
+    {
+        if (isalpha(c))
+        {
+            c = toupper(c);
+        }
+
+        else if (isdigit(c))
+        {
+            c = '*';
+
+        }
+        else if (c == ' ' || c == '\t')
+        {
+            c = '_';
+        }
+
+    }
+
+    cout << "Novi string je: " << s << endl;
+
+
+    //ZAD4
+
+    int niz[] = { 1,2,3,4,5 };
+    int duzina = 5;
+
+    at(niz, 2) += 1;
+
+    cout << "Promjenjeni niz glasi: ";
+    for (int i = 0; i < duzina; i++)
+    {
+        cout << niz[i];
+    }
+    cout << endl;
+
+    at(niz, 1) = 99;
+    at(niz, 4)++;
+
+
+    cout << "Promjenjeni niz glasi: ";
+    for (int i = 0; i < duzina; i++)
+    {
+        cout << niz[i];
+    }
+    cout << endl;
+
+    // ZAD1
+
+
+    bool flag;
+    cout << "Unesi " << true << " - " << false << ": ";
+    cin >> flag;
+    cout << boolalpha << flag << endl;
+
+    int num = 255;
+    cout << "hex " << hex << num << endl;
+    cout << "dec " << dec << num << endl;
+    cout << "oct " << oct << num << endl;
+
+    bool flag2;
+    cout << "Unesi " << true << " - " << false << ": ";
+    cin >> flag2;
+    cout << noboolalpha << flag2 << endl;
+
+    double pi = 3.141592;
+    cout << "pi = " << scientific << uppercase;
+    cout << setprecision(7) << setw(20);
+    cout << setfill('0');
+    cout << pi << endl;
+
+
+    return 0;
 }
-
-
-
-
-
-/*
-int main()
-{
-	bool flag;
-	std::cout << "unesi " << true << " - " << false << std::endl;
-	std::cin >> flag;
-	std::cout << std::boolalpha << flag << std::endl;
-
-	int a = 255;
-	std::cout << "hex " << std::hex << a << std::endl;
-	std::cout << "dec " << std::dec << a << std::endl;
-	std::cout << "oct " << std::oct << a << std::endl;
-
-	double pi = 3.141592;
-	std::cout << "pi = " << std::scientific << std::uppercase;
-	std::cout << std::setprecision(7) << std::setw(20);
-	std::cout << std::setfill('0');
-	std::cout << pi << std::endl;
-
-	bool flag2;
-	cout << "unesi " << true << " - " << false << endl;
-	cin >> flag;
-	cout << noboolalpha << flag << endl;
-
-}
-*/
