671e7f38eb1dc860cb17c5911ee6dc15752067ac
NikolaMacukatic <nikolamacukatic123@gmail.com>
2025-10-27 11:05:13 +0100

    Add files via upload
    Vjezba3 Ne dovrsena

---
+++ Vjezba3.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include <cstddef>
+#define void sortt(int[], std::size_t, bool (*cmp)(int, int));
+using namespace std;
+
+
+inline int asc(int a, int b)
+{
+	
+}
+
+
+inline int desc(int a, int b)
+{
+	
+}
+
+int main()
+{
+	int a = 5;
+	int b = 2;
+
+	return 0;
+}
