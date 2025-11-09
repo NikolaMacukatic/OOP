e4eb7ed5f23a7a9971c540356beb504f71cf30d2
NikolaMacukatic <nikolamacukatic123@gmail.com>
2025-11-09 23:22:31 +0100

    Vjezba4.cpp

--- Vjezba4.cpp
+++ Vjezba4.cpp
@@ -33,7 +33,81 @@ void print_vector(const vector<int>& v)
 
 }
 
+//TRECI
 
+
+void fix_spaces(string& text)
+{
+
+	auto prva = unique(text.begin(), text.end(), [](char a, char b) {return a == ' ' && b == ' '; });
+	text.erase(prva, text.end());
+
+
+	size_t pos = 0;
+	while ((pos = text.find(" ,", pos)) != std::string::npos) {
+		text.erase(pos, 1); 
+	}
+
+	pos = 0;
+	while ((pos = text.find(" .", pos)) != std::string::npos) {
+		text.erase(pos, 1); 
+	}
+
+	
+	pos = 0;
+	while ((pos = text.find(',', pos)) != std::string::npos) {
+		if (pos + 1 < text.length() && text[pos + 1] != ' ') {
+			text.insert(pos + 1, " ");
+		}
+		pos++;
+	}
+
+	
+	pos = 0;
+	while ((pos = text.find('.', pos)) != std::string::npos) {
+		if (pos + 1 < text.length() && text[pos + 1] != ' ') {
+			text.insert(pos + 1, " ");
+		}
+		pos++;
+	}
+
+	if (!text.empty() && text[0] == ' ') {
+		text.erase(0, 1);
+	}
+	if (!text.empty() && text[text.length() - 1] == ' ') {
+		text.erase(text.length() - 1, 1);
+	}
+
+}
+
+//CETVRTI
+
+
+string word_to_pig_latin(const string& word)
+{
+	if (word.empty())
+		return "";
+
+	char first = tolower(word[0]); 
+	string vowels = "aeiou";
+
+	
+	if (vowels.find(first) != string::npos) {
+		return word + "hay";
+	}
+	else {
+		return word.substr(1) + word[0] + "ay";
+	}
+}
+
+//PETI
+
+
+void reverse_strings(vector<string>& words) {
+	for (auto& word : words) {
+		reverse(word.begin(), word.end());
+	}
+}
 
 int main()
 {
@@ -62,7 +136,35 @@ int main()
 		return abs(a) < abs(b);
 	});
 	
-	
+	//Treci
+
+string text;
+cout << "Unesi recenicu: " << endl;
+cin.ignore();
+getline(cin, text);
+
+fix_spaces(text);
+
+cout << "Novi text glasi:" << text << endl;
+
+//Cetri
+
+cout << word_to_pig_latin("apple") << endl; 
+cout << word_to_pig_latin("hello") << endl; 
+cout << word_to_pig_latin("Table") << endl; 
+
+//Pet
+
+vector<string> words = { "hello", "world", "c++", "programming", "algorithm" };
+
+reverse_strings(words);
+
+cout << "Nakon okretanja:" << endl;
+for (const auto& word : words) {
+	cout << word << " ";
+}
+cout << endl;
 
 	return 0;
+
 }
